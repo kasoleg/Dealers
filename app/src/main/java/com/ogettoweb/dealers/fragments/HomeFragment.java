4441477d0b087a509117ae15d478fe24a79bec76
kasoleg1 <kasoleg1@gmail.com>
2016-08-06 19:18:13 +0300

    GITHUB-2 add getting current gps location

--- app/src/main/java/com/ogettoweb/dealers/fragments/HomeFragment.java
+++ app/src/main/java/com/ogettoweb/dealers/fragments/HomeFragment.java
@@ -1,20 +1,37 @@
 package com.ogettoweb.dealers.fragments;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.ogettoweb.dealers.R;
 import com.ogettoweb.dealers.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements LocationListener {
+    final int PERMISSION_LOCATION = 0;
+
     private FragmentHomeBinding binding;
+    private LocationManager locationManager;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -27,8 +44,98 @@ public class HomeFragment extends Fragment {
 
         SpannableString spanString = new SpannableString(binding.dealers.getText());
         spanString.setSpan(new UnderlineSpan(), 0, spanString.length() - 2, 0);
-        binding.dealers.setText(spanString );
+        binding.dealers.setText(spanString);
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+
+        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!enabled) {
+            new AlertDialog.Builder(getContext())
+                    .setTitle(R.string.app_name)
+                    .setMessage(R.string.coordinats_message)
+                    .setPositiveButton(R.string.enable, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton(R.string.exit, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            getActivity().finish();
+                        }
+                    }).show();
+        } else {
+            askForLocation();
+        }
+    }
+
+    private void askForLocation() {
+        if (isLocationAllowed(getContext())) {
+            String provider = locationManager.getBestProvider(new Criteria(), false);
+            locationManager.requestLocationUpdates(provider, 3000, 5, this);
+        } else {
+            requestLocationPermission();
+        }
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (isLocationAllowed(getContext())) {
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+        Toast.makeText(getContext(), "Latitude = " + lat + " Longitude = " + lng, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    private boolean isLocationAllowed(Context context) {
+        return ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermission() {
+        if (!isLocationAllowed(getContext())) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) && !ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_LOCATION: {
+                if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Toast.makeText(getActivity(), R.string.location_permission_denied, Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
     }
 }
